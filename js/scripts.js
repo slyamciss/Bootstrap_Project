51851ee41721361e969dd209d1cbf674037c1173
Souleymane Cisse <slyamciss@gmail.com>
2021-04-16 00:29:54 -0400

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,17 @@ $(function() {
               $("#carouselButton").children("i").addClass("fa-pause"); 
           }
       });
- });
+ 
+});
+
+ $(function() {
+    $("#reserveButton").click(function() {
+    $("#reserveModal").modal("show");
+   })
+})
+
+$(function() {
+    $("#Login").click(function() {
+    $("#loginModal").modal("show");
+   })
+})
